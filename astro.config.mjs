154b0f57d5b078a1c0e8922951683675be0005a9
Franco Lorenzo <franco14lorenzo@gmail.com>
2023-07-03 18:38:25 -0300

    🚀 Fix astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: vercel({
-    analytics: true,
-  }),
-});
+	integrations: [tailwind()],
+	output: 'static',
+	adapter: vercel({
+		analytics: true
+	})
+})
