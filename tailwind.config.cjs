1267c53bc7f9516b593caf1bff97642581c7e7f0
Franco Lorenzo <franco14lorenzo@gmail.com>
2025-10-22 09:07:44 -0300

    💄 Improve theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class', // Ensure dark mode is enabled via class
 	theme: {
 		extend: {
 			fontFamily: {
